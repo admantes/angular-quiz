f1f11a919cf710cb6bf14359187c51688778908e
Aris Mantes <admantes.com>
2019-08-13 20:32:08 +0800

    Added webcam lib support

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Webcam from 'webcamjs';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  choices;
 
-  constructor() { }
+  constructor() { 
+    this.choices = ["This is choice 1 of 4"];
+    
+  }
+
+  enableCam(){
+    //alert("setting webcam");
+
+    Webcam.set({
+			width: 320,
+			height: 240,
+			image_format: 'jpeg',
+			jpeg_quality: 90
+		});
+		Webcam.attach( '#my_camera' );
+  }
 
   ngOnInit() {
+    this.enableCam();
   }
 
 }
