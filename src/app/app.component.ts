1d8ec7cc0fa95f62db05fa47c2b396f975783666
Aris Mantes <admantes.com>
2019-09-18 14:45:42 +0800

    Added custom models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Question } from './models/Question';
 
 @Component({
   selector: 'app-root',
@@ -19,46 +20,7 @@ export class AppComponent implements OnInit {
   score;
   
   constructor( private http:HttpClient ){
-    this.questionIndex = 0;
-   /* this.questions = [
-      "Which of the following is a reserved keyword in C#?",
-      "Which of the following is correct about value type variables in C#?  ",
-      "Which of the following converts a type to a double type in C#?",
-      "Which of the following operator returns the size of a data type in C#?",
-      "Which of the following access specifier in C# allows a class to expose its member variables and member functions to other functions and objects?"
-    ];
-
-    this.choices = [
-      [ "abstract", "as", "foreach", "All of the above"] ,
-      [
-       "The value types directly contain data. ",
-       "int, char, and float, which stores numbers, alphabets, and floating point numbers, respectively are value types.", 
-       "When you declare an int type, the system allocates memory to store the value.", 
-       "All of the above"
-      ],
-      [
-        "ToDecimal",
-        "ToDouble",
-        "ToInt16",
-        "ToInt32"
-      ],
-      [
-        "sizeof",
-        "typeof",
-        "&</a>",
-        "*"
-      ], 
-      [
-        "Public",
-        "Private",
-        "Protected",
-        "Internal"
-      ],
-      ];
-
-
-      this.answers = [ 3, 3, 1, 0, 0 ];
-      */
+    this.questionIndex = 0;  
 
       this.finished = false;
       this.answered = false;
@@ -66,8 +28,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(){
-    this.http.get<Object>( '../assets/questions.json').subscribe(
+    this.http.get<Question>( '../assets/questions.json').subscribe(
       data => {
+        console.log(data);
         this.questions = data.questions;
         this.choices = data.choices;
         this.answers = data.answers;
@@ -83,11 +46,7 @@ export class AppComponent implements OnInit {
       this.score = 0;
   }
   nextQuestion(){
-    //alert("from app");
-   
-
-   
-
+    
     if(this.finished){
       //Retake the Quiz
       this.resetQuiz();
---
+++ src/app/models/Question.ts
@@ -0,0 +1,5 @@
+export class Question{
+    questions: Array<string>;
+    choices: Array<any>;
+    answers: Array<number>;
+}
